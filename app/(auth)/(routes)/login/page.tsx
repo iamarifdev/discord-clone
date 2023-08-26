36a9466998d53b3a9e81f889b486361169ec6d3c
Md. Ariful Islam <arifjmamun24@gmail.com>
2023-08-26 09:53:53 +0600

    refactor folder structures

---
+++ app/(auth)/(routes)/login/page.tsx
@@ -0,0 +1,7 @@
+const LoginPage = () => {
+  return (
+    <div>Login Page</div>
+  );
+}
+ 
+export default LoginPage;
---
+++ app/(auth)/(routes)/register/page.tsx
@@ -0,0 +1,7 @@
+const RegisterPage = () => {
+  return (
+    <div>Register Page</div>
+  );
+}
+ 
+export default RegisterPage;
